5055abf4c6411d168d1fe94d6d568c7d16b24786
10q4attention <10q4attention@mail.ru>
2024-02-24 23:15:10 +0300

    finished Slider.vue

--- src/components/elements/main/Slider.vue
+++ src/components/elements/main/Slider.vue
@@ -1,3 +1,44 @@
 <template>
- дядька slider
-</template>
+    <swiper
+      :effect="'cards'"
+      :grabCursor="true"
+      :modules="modules"
+      class="mySwiper"
+    >
+      <swiper-slide><img src="../../../../public/Img/slider-pic1.jpg" class="slide-image"></swiper-slide>
+      <swiper-slide><img src="../../../../public/Img/slider-pic2.jpg" class="slide-image"></swiper-slide>
+      <swiper-slide><img src="../../../../public/Img/slider-pic3.jpg" class="slide-image"></swiper-slide>
+      <swiper-slide><img src="../../../../public/Img/slider-pic4.jpg" class="slide-image"></swiper-slide>
+
+
+    </swiper>
+  </template>
+  
+  <script>
+    // Import Swiper Vue.js components
+    
+    import { Swiper, SwiperSlide } from 'swiper/vue';
+  
+    // Import Swiper styles
+    import 'swiper/css';
+  
+    import 'swiper/css/effect-cards';
+  
+    import './style.css';
+  
+    // import required modules
+    import { EffectCards } from 'swiper/modules';
+  
+    export default {
+      components: {
+        Swiper,
+        SwiperSlide,
+      },
+      setup() {
+        return {
+          modules: [EffectCards],
+        };
+      },
+    };
+  </script>
+  
--- src/pages/IamPage.vue
+++ src/pages/IamPage.vue
@@ -3,8 +3,11 @@
         <BackButton />
         <IAm />
     </div>
+
+    <Slider />
 </template>
 <script setup>
     import BackButton from '@/components/ui/BackButton.vue';
     import IAm from '@/components/elements/iam/IAm.vue';
+    import Slider from '@/components/elements/main/Slider.vue';
 </script>
