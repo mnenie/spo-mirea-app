1474c0e069d334fb50be96203e8aeea6a3d0a87a
mnenie <1alexpeshkov@gmail.com>
2024-03-05 21:15:33 +0300

    fix: mock bd

--- src/mocks/users.mock.js
+++ src/mocks/users.mock.js
@@ -1,14 +1,84 @@
 import { ref } from 'vue'
 
 export const users = ref([
-  { id: 0, name: 'Иван', lastname: 'Молчанов', gender: 'Мужской', city: 'Москва' },
-  { id: 1, name: 'Данила', lastname: 'Холкин', gender: 'Мужской', city: 'Москва' },
-  { id: 2, name: 'Алексей', lastname: 'Позиев', gender: 'Мужской', city: 'Санкт-Петербург' },
-  { id: 3, name: 'Эрик', lastname: 'Григорьев', gender: 'Мужской', city: 'Санкт-Петербург' },
-  { id: 4, name: 'Александр', lastname: 'Пешков', gender: 'Мужской', city: 'Москва' },
-  { id: 5, name: 'Рустам', lastname: 'Уралович', gender: 'Мужской', city: 'Москва' },
-  { id: 6, name: 'Дарья', lastname: 'Щетинина', gender: 'Женский', city: 'Москва' },
-  { id: 7, name: 'Анастасия', lastname: 'Мокшина', gender: 'Женский', city: 'Москва' },
-  { id: 8, name: 'Александра', lastname: 'Матеюк', gender: 'Женский', city: 'Санкт-Петербург' },
-  { id: 9, name: 'Галина', lastname: 'Рядом', gender: 'Женский', city: 'Санкт-Петербург' }
+  {
+    id: 0,
+    name: 'Иван',
+    lastname: 'Молчанов',
+    gender: 'Мужской',
+    city: 'Москва',
+    img: '/Img/molchanov.png'
+  },
+  {
+    id: 1,
+    name: 'Данила',
+    lastname: 'Холкин',
+    gender: 'Мужской',
+    city: 'Москва',
+    img: '/Img/xolkin.png'
+  },
+  {
+    id: 2,
+    name: 'Алексей',
+    lastname: 'Позиев',
+    gender: 'Мужской',
+    city: 'Санкт-Петербург',
+    img: '/Img/poziev.png'
+  },
+  {
+    id: 3,
+    name: 'Эрик',
+    lastname: 'Григорьев',
+    gender: 'Мужской',
+    city: 'Санкт-Петербург',
+    img: '/Img/eric.png'
+  },
+  {
+    id: 4,
+    name: 'Александр',
+    lastname: 'Пешков',
+    gender: 'Мужской',
+    city: 'Москва',
+    img: '/Img/peshkov.png'
+  },
+  {
+    id: 5,
+    name: 'Рустам',
+    lastname: 'Уралович',
+    gender: 'Мужской',
+    city: 'Москва',
+    img: '/Img/slider-pic2.png'
+  },
+  {
+    id: 6,
+    name: 'Дарья',
+    lastname: 'Щетинина',
+    gender: 'Женский',
+    city: 'Москва',
+    img: '/Img/dora.png'
+  },
+  {
+    id: 7,
+    name: 'Анастасия',
+    lastname: 'Мокшина',
+    gender: 'Женский',
+    city: 'Москва',
+    img: '/Img/nastya.png'
+  },
+  {
+    id: 8,
+    name: 'Александра',
+    lastname: 'Матеюк',
+    gender: 'Женский',
+    city: 'Санкт-Петербург',
+    img: '/Img/alexandra.png'
+  },
+  {
+    id: 9,
+    name: 'Галина',
+    lastname: 'Рядом',
+    gender: 'Женский',
+    city: 'Санкт-Петербург',
+    img: '/Img/galina.png'
+  }
 ])
