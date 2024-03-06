66ad3b1a2858d1ff7c71ba340eb182c00848e0ee
mnenie <1alexpeshkov@gmail.com>
2024-03-06 18:57:50 +0300

    add: computed property in filters

--- src/stores/user.store.js
+++ src/stores/user.store.js
@@ -4,14 +4,16 @@ import { users } from '@/mocks/users.mock'
 
 export const useUsers = defineStore('users', () => {
   const usersArr = ref(users.value)
-  const filterUsers = (filters) => {
-    usersArr.value = users.value.filter((user) => {
-      return (
-        (!filters.gender || user.gender === filters.gender) &&
-        (!filters.city || user.city === filters.city)
-      );
-    });
-  };
+  const filterUsers = computed(() => {
+    return (filters) => {
+      usersArr.value = users.value.filter((user) => {
+        return (
+          (!filters.gender || user.gender === filters.gender) &&
+          (!filters.city || user.city === filters.city)
+        )
+      })
+    }
+  })
   return {
     usersArr,
     filterUsers
